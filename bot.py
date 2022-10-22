6adca47fd4eb0042e011ae615e26caaa3aa3806a
Никита Вишневский <nikitavish@list.ru>
2022-10-23 01:45:37 +0300

    callback added

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
+import json
+import requests
 import telebot
 import os
+from keyboa import Keyboa
+from telebot import types
 from telebot.async_telebot import AsyncTeleBot
+
+import web_markup
 from web_markup import web_app_keyboard
 
 from dotenv import load_dotenv
@@ -12,13 +18,31 @@ bot = AsyncTeleBot(os.getenv('API_TOKEN'))
 
 @bot.message_handler(commands=['start'])
 async def start(msg):
-    await bot.send_message(msg.chat.id, "Hello world!")
+    keyboard = types.ReplyKeyboardRemove()
+    await bot.send_message(msg.chat.id, "Hello world!", reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['web'])
 async def web_start(msg):
     await bot.send_message(msg.chat.id, "НУ ты!!!...", reply_markup=web_app_keyboard())
 
+
+@bot.message_handler(commands=['forms'])
+async def forms_start(msg):
+    # forms = ["Dorogi"]
+    forms = requests.get('https://miet-hack-api.herokuapp.com/form-names').json()['names']
+    keyboard = Keyboa(items=forms)
+    await bot.send_message(msg.chat.id, "Выберите форму:", reply_markup=keyboard())
+    bot.register_callback_query_handler(choose_form_callback, None)
+
+
+async def choose_form_callback(callback: types.CallbackQuery):
+    form_name = callback.data
+    chat_id = callback.from_user.id
+    await bot.send_message(chat_id, "Можете заполнять форму", reply_markup=web_markup.form_open(form_name))
+
+
 if __name__ == '__main__':
     import asyncio
     asyncio.run(bot.polling())
+
--- web_markup.py
+++ web_markup.py
@@ -1,10 +1,22 @@
+import requests
+from keyboa import Keyboa
 from telebot import types
 
 
 def web_app_keyboard():  # создание клавиатуры с webapp кнопкой
     keyboard = types.ReplyKeyboardMarkup(row_width=1)  # создаем клавиатуру
-    web_app_test = types.WebAppInfo("https://telegram.mihailgok.ru")  # создаем webappinfo - формат хранения url
+    web_app_test = types.WebAppInfo("https://f5uhuv.deta.dev/forms/Dorogi/new")  # создаем webappinfo - формат хранения url
     one_btn = types.KeyboardButton(text="Тестовая страница", web_app=web_app_test)  # создаем кнопку типа webapp
     keyboard.add(one_btn)  # добавляем кнопки в клавиатуру
 
     return keyboard  # возвращаем клавиатуру
+
+
+def form_open(form_name):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1)  # создаем клавиатуру
+    keyboard = types.InlineKeyboardMarkup()
+    web_app_test = types.WebAppInfo(f"https://f5uhuv.deta.dev/forms/{form_name}/new")  # создаем webappinfo - формат хранения url
+
+    one_btn = types.InlineKeyboardButton(text="Форма", web_app=web_app_test)  # создаем кнопку типа webapp
+    keyboard.add(one_btn)  # добавляем кнопки в клавиатуру
+    return keyboard
